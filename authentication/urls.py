02fc8daa3057e36033e1c3f572941c8ab9825a0b
devorder <satyam.sharmadev@gmail.com>
2022-01-06 12:37:29 +0530

    started register form

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterationView.as_view(), name="register"),
+    path('validate-user/', views.UserNameValidationView.as_view(), name="validate-user"),
+    path('login/', views.RegisterationView.as_view(), name="login"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,24 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 class RegisterationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+class UserNameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({
+                'status': 1,
+                'message': 'username should only contain alphanumeric character.',
+                })
+        return JsonResponse({
+                'status': 0,
+                'message': 'username valid.',
+                })
